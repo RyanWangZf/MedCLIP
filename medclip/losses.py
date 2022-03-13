5ff9efc9b4f030962f314f35ea9b654824474e7c
Zifeng Wang <zifengw2@illinois.edu>
2022-03-13 12:46:02 -0500

    three losses and trainer, TODO: evaluation and data split

--- medclip/losses.py
+++ medclip/losses.py
@@ -41,3 +41,89 @@ class ImageTextContrastiveLoss(nn.Module):
                 return_dict=return_dict,
                 )
         return outputs.loss
+
+class ImageImageContrastiveLoss(nn.Module):
+    '''compute contrastive loss using momentum memory banks between images and images,
+    MOCO-V3 like.
+    '''
+    def __init__(self, model, momentum_model, T=1):
+        '''
+        T: temperature for infonce loss
+        '''
+        super().__init__()
+        self.base_encoder = model
+        self.momentum_encoder = momentum_model
+        self.T = T
+        # build momentum encoder
+        for param_b, param_m in zip(self.base_encoder.parameters(), self.momentum_encoder.parameters()):
+            param_m.data.copy_(param_b.data)  # initialize
+            param_m.requires_grad = False  # not update by gradient
+            param_m.to(param_b.device)
+
+    @torch.no_grad()
+    def _update_momentum_encoder(self, m):
+        """Momentum update of the momentum encoder"""
+        for param_b, param_m in zip(self.base_encoder.parameters(), self.momentum_encoder.parameters()):
+            param_m.data = param_m.data * m + param_b.data * (1. - m)
+
+    def contrastive_loss(self, q, k):
+        # Einstein sum is more intuitive
+        logits = torch.einsum('nc,mc->nm', [q, k]) / self.T
+        N = logits.shape[0]  # batch size per GPU
+        labels = torch.arange(N, dtype=torch.long).cuda()
+        loss = nn.CrossEntropyLoss()(logits, labels) * (2 * self.T)
+        return loss
+
+    def forward(self,
+        input_ids=None,
+        pixel_values=None,
+        labels=None,
+        attention_mask=None,
+        position_ids=None,
+        return_loss=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        '''
+        * for normal/abnormal CL loss
+            x1: normal images, x2: abnormal images
+            q1: normal image feature, q2: abnormal image feature
+            k1: normal features using momentum encoder
+            k2: abnormal features using momentum encoder
+            CL loss is applied for:
+            negative pairs: {q1,k2}, {q2,k1}
+            positive pairs: {q1,k1}, {q2,k2}
+            to cl loss, it is {q1, k1, k2} and {q2, k2, k1}
+
+        * for frontal/lateral CL loss:
+            x1: frontal, x2: lateral
+            q1, q2: encode x1, x2
+            k1, k2: momentum encode x1, x2
+            same as MOCO where {q1,k2} positive, {q2,k1} positive on diagonal
+        '''
+        feat_base = self.base_encoder.encode_image(pixel_values) # bs, 512
+        feat_momtm = self.momentum_encoder.encode_image(pixel_values) # bs, 512
+
+        if len(labels.unique()) == 1: # no abnormal images / normal images
+            # only do instance discriminative loss
+            loss = self.contrastive_loss(feat_base, feat_momtm)
+            return loss
+        else:
+            feat_base_nm = feat_base[labels==0]
+            feat_base_ab = feat_base[labels==1]
+            feat_momtm_nm = feat_momtm[labels==0]
+            feat_momtm_ab = feat_momtm[labels==1]
+
+            # q1,k1,k2
+            loss_nm = self.contrastive_loss(
+                feat_base_nm, 
+                torch.cat([feat_momtm_nm, feat_momtm_ab], axis=0)
+                )
+
+            # q2,k2,k1
+            loss_ab = self.contrastive_loss(
+                feat_base_ab, 
+                torch.cat([feat_momtm_ab, feat_momtm_nm], axis=0)
+                )
+            return loss_ab + loss_nm
--- run_medclip_cl.py
+++ run_medclip_cl.py
@@ -9,15 +9,16 @@ import torch
 import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
-from transformers import CLIPProcessor, CLIPModel
 
-from medclip.dataset import IUXRayDataset, IUXRayImageTextCollator, IUXRayAbnormalNormalCollator
+from medclip.dataset import IUXRayDataset, IUXRayImageTextCollator, IUXRayAbnormalNormalCollator, IUXRayFrontalLateralCollator
 from medclip.modeling_clip import MedCLIPModel
-from medclip.losses import ImageTextContrastiveLoss
+from medclip.losses import ImageTextContrastiveLoss, ImageImageContrastiveLoss
 from medclip.trainer import Trainer
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
 train_config = {
-    'train_batch_size': 16, # 16 * 5 = 80
+    'batch_size': 16,
     'num_epochs': 1,
     'warmup': 0.1, # the first 10% of training steps are used for warm-up
     'lr': 1e-5,
@@ -28,32 +29,39 @@ if not os.path.exists(model_save_path):
     os.makedirs(model_save_path)
 
 # #########
-# define three contrastive loss models?
+# define three contrastive loss models
 # #########
 model = MedCLIPModel()
+model = model.to(device)
+momentum_model = MedCLIPModel()
+momentum_model = momentum_model.to(device)
 
 # image-text pair CL
 training_data = IUXRayDataset('./data/IU_XRay')
 collate_fn = IUXRayImageTextCollator(img_mean=training_data.img_mean, img_std=training_data.img_std, is_train=True)
-dataloader = DataLoader(training_data, batch_size=4, shuffle=False, collate_fn=collate_fn)
+dataloader_image_text = DataLoader(training_data, batch_size=train_config['batch_size'], shuffle=True, collate_fn=collate_fn)
 train_loss_image_text = ImageTextContrastiveLoss(model)
 warmup_steps = math.ceil(len(training_data) * train_config['num_epochs'] * train_config['warmup']) #10% of train data for warm-up
 
-# three loss models
-# abnormal-normal pair CL + memory banking
-# training_data = IUXRayDataset('./data/IU_XRay')
-# collate_fn = IUXRayAbnormalNormalCollator(img_mean=training_data.img_mean, img_std=training_data.img_std, is_train=True)
-# dataloader = DataLoader(training_data, batch_size=4, shuffle=False, collate_fn=collate_fn)
+# abnormal-normal pair CL + memory banking (moco V3)
+training_data = IUXRayDataset('./data/IU_XRay')
+collate_fn = IUXRayAbnormalNormalCollator(img_mean=training_data.img_mean, img_std=training_data.img_std, is_train=True)
+dataloader_abnormal = DataLoader(training_data, batch_size=train_config['batch_size'], shuffle=True, collate_fn=collate_fn)
+train_loss_abnormal = ImageImageContrastiveLoss(model, momentum_model)
 
-# test data collator
-# for batch in dataloader:
-#     pass
+# frontal-lateral paired CL + memory banks (moco V3)
+training_data = IUXRayDataset('./data/IU_XRay')
+collate_fn = IUXRayFrontalLateralCollator(img_mean=training_data.img_mean, img_std=training_data.img_std, is_train=True)
+dataloader_frontal = DataLoader(training_data, batch_size=train_config['batch_size'], shuffle=True, collate_fn=collate_fn)
+train_loss_frontal = ImageImageContrastiveLoss(model, momentum_model)
 
-# frontal-lateral pair CL
 train_objectives = [
-    (dataloader, train_loss_image_text),
+    (dataloader_image_text, train_loss_image_text),
+    (dataloader_abnormal, train_loss_abnormal),
+    (dataloader_frontal, train_loss_frontal),
 ]
 
+# TODO fix checkpoint save and evaluation in trainer
 trainer = Trainer()
 trainer.train(
     model,
@@ -61,7 +69,6 @@ trainer.train(
     warmup_steps=warmup_steps,
     epochs=train_config['num_epochs'],
     optimizer_params={'lr':train_config['lr']},
-    batch_size=train_config['train_batch_size'],
     output_path=model_save_path,
     checkpoint_path=model_save_path,
     checkpoint_save_steps=1000,
